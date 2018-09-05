31142a91b8b4bdd309c0d374ff44e1142704007d
Daniel King <daniel.zidan.king@gmail.com>
2018-09-05 10:15:48 -0400

    fix

--- hail-ci-deploy.sh
+++ hail-ci-deploy.sh
@@ -1,5 +1,5 @@
 set +ex
 gcloud auth activate-service-account \
-  --key-file=/secrets/${GCLOUD_ACCT_NAME}.json
+  --key-file=/secrets/ci-deploy-0-1--hail-is-ci-test-service-account-key.json
 git show HEAD > foo
 gsutil cp foo gs://hail-ci-test/$(git rev-parse HEAD)
